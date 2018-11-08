fa0a05a9106de2492e7625d9a117d4b7e89a5ad5
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2018-11-08 14:39:15 +0100

    Remove nest form.

--- src/neuron-layer/implementation.lisp
+++ src/neuron-layer/implementation.lisp
@@ -14,22 +14,21 @@
   ;; select predictive neurons
   ;; set active neurons
   ;; finally, return all predictive neurons
-  (nest
-   (let* ((columns (read-columns layer))
-          (column-indices (read-column-indices layer))
-          (activate-neurons-count (read-activated-neurons-count columns))
-          (activated-columns-count (read-activated-columns-count columns))
-          (active-synapses-for-columns (calculate-active-synapses-for-columns
-                                        columns sdr))
-          (active-columns (select-active-columns columns
-                                                 active-synapses-for-columns))
-          (predictive-neurons (select-predictive-neurons layer
-                                                         columns
-                                                         active-columns))
-          (active-neurons (select-active-neurons layer
-                                                 columns
-                                                 active-columns
-                                                 prev-data)))
-     (cl-htm.sdr:set-active layer active-neurons 1)
-     (update-synapses layer columns active-columns prev-data active-neurons)
-     predictive-neurons)))
+  (let* ((columns (read-columns layer))
+         (column-indices (read-column-indices layer))
+         (activate-neurons-count (read-activated-neurons-count columns))
+         (activated-columns-count (read-activated-columns-count columns))
+         (active-synapses-for-columns (calculate-active-synapses-for-columns
+                                       columns sdr))
+         (active-columns (select-active-columns columns
+                                                active-synapses-for-columns))
+         (predictive-neurons (select-predictive-neurons layer
+                                                        columns
+                                                        active-columns))
+         (active-neurons (select-active-neurons layer
+                                                columns
+                                                active-columns
+                                                prev-data)))
+    (cl-htm.sdr:set-active layer active-neurons 1)
+    (update-synapses layer columns active-columns prev-data active-neurons)
+    predictive-neurons))

1a8ba0be8a7adaa6813ffc6d411ff233ad29ca8c
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2018-12-19 11:20:51 +0100

    Change class once range is empty.

--- src/utils/lazy-vector.lisp
+++ src/utils/lazy-vector.lisp
@@ -15,24 +15,28 @@
 (defgeneric at (vector position))
 
 
-(defgeneric (setf at) (vector position))
-
-
 (defmethod at ((vector lazy-vector) position)
   (aref (the simple-vector (slot-value vector '%inner-vector)) position))
 
 
 (defmethod at ((vector potential-lazy-vector) position)
-  (let* ((vector (slot-value vector '%inner-vector))
-         (length (array-dimension vector 0)))
+  (let* ((inner-vector (slot-value vector '%inner-vector))
+         (length (array-dimension inner-vector 0)))
     (declare (type simple-vector vector)
              (type fixnum length))
     (unless (< position length)
-      (iterate
-        (with range = (read-data-range vector))
-        (for i from length to position)
-        (for (values v m) = (cl-ds:consume-front range))
-        (unless m
-          (error "Not enough data to fill the vector!"))
-        (setf (aref vector i) v)))
+      (let ((new-vector (make-array (+ position 1)
+                                    :element-type (array-element-type inner-vector)))
+            (range (read-data-range vector)))
+        (map-into new-vector #'identity inner-vector)
+        (iterate
+          (with range = (read-data-range vector))
+          (for i from length to position)
+          (for (values v m) = (cl-ds:consume-front range))
+          (unless m
+            (error "Not enough data to fill the vector!"))
+          (setf (aref new-vector i) v))
+        (unless (~>> range cl-ds:peek-front (nth-value 1))
+          (change-class vector 'lazy-vector))
+        (setf (slot-value vector '%inner-vector) new-vector)))
     (call-next-method)))
